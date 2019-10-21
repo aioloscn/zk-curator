c72087c22138d76a5e8188cd8254af113a1f10b3
aioloscn <756243011@qq.com>
2019-10-21 16:57:35 +0800

    'deleteNode'

--- src/main/java/com/aiolos/curator/CuratorOperator.java
+++ src/main/java/com/aiolos/curator/CuratorOperator.java
@@ -8,6 +8,8 @@ import org.apache.curator.framework.recipes.cache.PathChildrenCache;
 import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
 import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
 import org.apache.curator.retry.*;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs;
 
 import java.util.List;
 
@@ -73,11 +75,32 @@ public class CuratorOperator {
             this.client.close();
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
 
         CuratorOperator cto = new CuratorOperator();
         boolean isZkCuratorStarted = cto.client.isStarted();
         System.out.printf("当前客户的状态：%s\n", isZkCuratorStarted ? "连接中" : "已关闭");
+
+        // 1. 创建节点
+        String nodePath = "/super/aiolos";
+        /*byte[] data = "superme".getBytes();
+        cto.client.create().creatingParentsIfNeeded()
+                .withMode(CreateMode.PERSISTENT)
+                .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)   // Access Control List
+                .forPath(nodePath, data);*/
+
+        // 2. 更新节点数据
+        /*byte[] newData = "batman".getBytes();
+        cto.client.setData()
+                .withVersion(0) // 对应client中的dataVersion，修改后变为1
+                .forPath(nodePath, newData);*/
+
+        // 3. 删除节点
+        cto.client.delete()
+                .guaranteed()   // 如果删除失败，那么在后端还是继续会删除，知道成功
+                .deletingChildrenIfNeeded() // 如果有子节点，就删除
+                .withVersion(1)
+                .forPath(nodePath);
 
         Thread.sleep(3000);
 
